adb44f2cbad953e5d1d53d37baaf9b0e0778139d
Sasha Petelin <alexanderzr58@icloud.com>
2022-10-03 22:45:54 +0300

    Homework9_C#_Petelin

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите значение натурального числа М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение натурального числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n) Console.WriteLine("М должен быть меньше N. Введите значения повторно!");
+
+Console.Write("Сумма чисел от М до N равна ");
+Console.WriteLine(GetSum(m));
+
+int GetSum(int m)
+{
+    if (m <= n) 
+    {
+        return (m + GetSum(m+1));;
+    }
+    return 0;
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Для вычисления функции Аккермана необходимо 2 неотрицательных числа.");
+Console.WriteLine();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Значение функции: ");
+Console.WriteLine(Akkerman(n,m));
+
+
+int Akkerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (m == 0) return Akkerman(n - 1, 1);
+    if (n > 0 && m > 0) return Akkerman(n - 1, Akkerman(n, m - 1));
+    return Akkerman(n, m);
+}
